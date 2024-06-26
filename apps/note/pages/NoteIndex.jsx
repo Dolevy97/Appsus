e119e897dd18f799434b7f979179ff11d5e88a49
Yonatan Hershko <yonher8@gmail.com>
2024-06-26 11:51:43 +0300

    New starting notes

--- apps/note/pages/NoteIndex.jsx
+++ apps/note/pages/NoteIndex.jsx
@@ -1,13 +1,17 @@
 
+// import { noteService } from "../services/note.service"
+// import { BookList } from "../cmps/NoteList.jsx"
 
 
-
-
+// const { Link ,useSearchParams} = ReactRouterDOM
+// const { useState, useEffect, useRef } = React
 
 
 
 
 
 export function NoteIndex() {
+
+    
     return <div>note app</div>
 }
--- apps/note/services/note.service.js
+++ apps/note/services/note.service.js
@@ -1,1 +1,39 @@
 // note service
+
+import { storageService } from '../../../services/async-storage.service.js'
+import { utilService } from '../../../services/util.service.js'
+
+
+export const noteService = {
+    noteTest,
+    // query,
+}
+
+
+
+
+
+function query() {
+
+}
+
+
+function noteTest() {
+
+const dummyNotes = [
+    {
+        id: 'n101',
+        createdAt: 1112222,
+        type: 'NoteTxt',
+        isPinned: true,
+        style: {
+            backgroundColor: '#00d'
+        },
+        info: {
+            txt: 'Fullstack Me Baby!'
+        }
+    },
+
+]
+return dummyNotes
+}
