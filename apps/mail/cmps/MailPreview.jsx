f3b63483a3198ccd7b50f0f45e9809c2968d89c0
Dolev Levy <Dolevlevy97@gmail.com>
2024-06-26 22:44:09 +0300

    Properly implement rendering

--- apps/mail/cmps/MailPreview.jsx
+++ apps/mail/cmps/MailPreview.jsx
@@ -3,17 +3,28 @@ import { mailService } from "../services/mail.service.js"
 const { Link, NavLink, useNavigate } = ReactRouterDOM
 const { useState, useEffect } = React
 
-export function MailPreview({ mail, getFormattedTime }) {
-    const [isStarred, setIsStarred] = useState(mail.isStarred)
+export function MailPreview({ mail, getFormattedTime, onSetMail }) {
     const [isRemovedAt, setIsRemovedAt] = useState(mail.removedAt)
-    const [isRead, setIsRead] = useState(mail.isRead)
+
     const navigate = useNavigate()
 
     function onSetStar(ev) {
         ev.stopPropagation()
-        const newMail = { ...mail, isStarred: !isStarred }
+        const newMail = { ...mail, isStarred: !mail.isStarred }
         mailService.save(newMail)
-            .then(setIsStarred(!isStarred))
+            .then(prevMail => {
+                onSetMail(prevMail)
+            })
+            .catch(err => console.log('Oh no! err:', err))
+    }
+
+    function onUpdateRead(ev) {
+        ev.stopPropagation()
+        const newMail = { ...mail, isRead: !mail.isRead }
+        mailService.save(newMail)
+            .then(prevMail => {
+                onSetMail(prevMail)
+            })
             .catch(err => console.log('Oh no! err:', err))
     }
 
@@ -25,15 +36,6 @@ export function MailPreview({ mail, getFormattedTime }) {
             .then(() => setIsRemovedAt(now))
             .catch(err => console.log('Oh no! err:', err))
     }
-
-    function onUpdateRead(ev) {
-        ev.stopPropagation()
-        const newMail = { ...mail, isRead: !isRead }
-        mailService.save(newMail)
-            .then(setIsRead(!isRead))
-            .catch(err => console.log('Oh no! err:', err))
-    }
-
 
     function moveToMail(mailId) {
         navigate(`/mail/${mailId}`)
@@ -53,7 +55,7 @@ export function MailPreview({ mail, getFormattedTime }) {
         return null;
     }
 
-    const isStar = isStarred ? 'star' : ''
+    const isStar = mail.isStarred ? 'star' : ''
     return (
         <div onClick={() => moveToMail(mail.id)} className={`flex space-between mail-item ${mail.isRead ? 'read' : ''}`}>
             <section className="mail-start flex align-center">
@@ -68,7 +70,7 @@ export function MailPreview({ mail, getFormattedTime }) {
             <span className="mail-sent-at">{getFormattedTime(mail.sentAt)}</span>
             <div className="hover-icons">
                 <span onClick={onDeleteMail} className="material-symbols-outlined hover-icon">delete</span>
-                <span onClick={onUpdateRead} className="material-symbols-outlined hover-icon">{isRead ? "mark_email_unread" : "mark_email_read"}</span>
+                <span onClick={onUpdateRead} className="material-symbols-outlined hover-icon">{mail.isRead ? "mark_email_unread" : "mark_email_read"}</span>
             </div>
         </div>
     )
