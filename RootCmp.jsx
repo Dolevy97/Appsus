794fc19b41f98479967745aed96407cdc30f8899
Dolev Levy <Dolevlevy97@gmail.com>
2024-06-26 09:12:53 +0300

    First Init

---
+++ RootCmp.jsx
@@ -0,0 +1,24 @@
+const { Route, Routes } = ReactRouterDOM
+const Router = ReactRouterDOM.HashRouter
+
+import { AppHeader } from "./cmps/AppHeader.jsx"
+import { About } from "./pages/About.jsx"
+import { Home } from "./pages/Home.jsx"
+import { MailIndex } from "./apps/mail/pages/MailIndex.jsx"
+import { NoteIndex } from "./apps/note/pages/NoteIndex.jsx"
+
+
+
+export function App() {
+    return <Router>
+        <section className="app">
+            <AppHeader />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/mail" element={<MailIndex />} />
+                <Route path="/note" element={<NoteIndex />} />
+            </Routes>
+        </section>
+    </Router>
+}
