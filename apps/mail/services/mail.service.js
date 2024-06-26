58dffa18ffe59a02af7e112f65a0e6653d5090df
Dolev Levy <Dolevlevy97@gmail.com>
2024-06-26 11:20:38 +0300

    Change storage.service export to 'localStorageService'

--- apps/mail/services/mail.service.js
+++ apps/mail/services/mail.service.js
@@ -1,1 +1,26 @@
-// mail service
+import { utilService } from '../../../services/util.service.js'
+import { storageService } from '../../../services/async-storage.service.js'
+
+const loggedinUser = {
+    email: 'user@appsus.com',
+    fullname: 'Dolev Levy'
+}
+
+// Private Functions
+
+function _createEmails() {
+    let email = storageService
+}
+
+function _createEmail() {
+    return {
+        id: 'e101',
+        subject: 'Miss you!',
+        body: 'Would love to catch up sometimes',
+        isRead: false,
+        sentAt: null,
+        removedAt: null,
+        from: 'momo@momo.com',
+        to: 'user@appsus.com'
+    }
+}
