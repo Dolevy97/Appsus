d4ae1e319f8af9d7b20ae6ce8953cc8d4c8843dc
Yonatan Hershko <yonher8@gmail.com>
2024-06-29 19:09:19 +0300

    note over-flow fix

--- apps/note/pages/NoteIndex.jsx
+++ apps/note/pages/NoteIndex.jsx
@@ -7,6 +7,7 @@ import { NoteList } from "../cmps/NoteList.jsx"
 import { NoteFilter } from "../cmps/NoteFilter.jsx"
 import { AddNote } from "../cmps/AddNote.jsx"
 import { NoteEdit } from "../pages/NoteEdit.jsx"
+import { showGmailMsg } from "../../../services/event-bus.service.js"
 
 const { Link, useSearchParams } = ReactRouterDOM
 const { useState, useEffect, useRef } = React
@@ -42,9 +43,15 @@ export function NoteIndex() {
 
     function onRemoveNote(noteId) {
         noteService.remove(noteId).then(() => {
-            setNotes((prevNotes) => prevNotes.filter((note) => note.id !== noteId))
+            setNotes((prevNotes) => {
+                prevNotes.filter((note) => note.id !== noteId)
+                
+                showGmailMsg('Note ')
+            })
         })
     }
+
+console.log(notes);
 
     function onSetFilter(filterBy) {
         setFilterBy({ ...filterBy })
